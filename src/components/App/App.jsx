361c55782d4daadd398406891cf8fc8b0867014a
Tamira2804 <tamira2804@gmail.com>
2023-08-21 21:39:30 +0300

    App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,70 @@
 import { Component } from 'react';
+import Searchbar from '../Searchbar';
+import Loader from 'components/Loader/Loader';
+import Button from '../Button';
+import ImageGallery from '../ImageGallery';
+import Modal from '../Modal';
+import { fetchImages } from '../../services/HttpService';
 
-import './App.scss';
+import { GlobalStyle } from '../GlobalStyle';
+import { Layout } from './App.styled';
+import './App.styled.js';
 
 class App extends Component {
   state = {
-    // contacts: [ ],
-    // filter: '',
+    query: '',
+    images: [],
+    page: 1,
+    loading: false,
+    selectedImage: null,
+  };
+
+  handleSearchSubmit = query => {
+    this.setState({ query, images: [], page: 1 }, this.fetchImages);
+  };
+
+  fetchImages = () => {
+    const { query, page } = this.state;
+
+    this.setState({ loading: true });
+
+    fetchImages(query, page)
+      .then(response => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...response.data.hits],
+          page: prevState.page + 1,
+        }));
+      })
+      .catch(error => console.error('Error fetching images:', error))
+      .finally(() => {
+        this.setState({ loading: false });
+      });
+  };
+
+  handleImageClick = imageUrl => {
+    this.setState({ selectedImage: imageUrl });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ selectedImage: null });
   };
 
   render() {
+    const { images, loading, selectedImage } = this.state;
+
     return (
-      <div className="App__container">
-        <h1>Phonebook</h1>
-      </div>
+      <Layout>
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery images={images} onItemClick={this.handleImageClick} />
+        {loading && <Loader />}
+        {images.length > 0 && !loading && (
+          <Button onClick={this.fetchImages} label="Load more" />
+        )}
+        {selectedImage && (
+          <Modal image={selectedImage} onClose={this.handleCloseModal} />
+        )}
+        <GlobalStyle />
+      </Layout>
     );
   }
 }
